fdccb98bd53bf9cc2a0a81eab1611b58e7504dc3
Martha-Zhao <1839394609@qq.com>
2019-11-15 10:27:41 +0800

    imu_filter node setup

---
+++ src/my_robot/include/my_robot/imu_filter.h
@@ -0,0 +1,143 @@
+#ifndef IMU_FILTER_H
+#define IMU_FILTER_H
+
+#include "ros/ros.h"
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/MagneticField.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <tf/tf.h>
+#include <deque>
+#include <eigen3/Eigen/Geometry> 
+#include <chrono>
+#include <locale>
+#include <tuple>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <boost/assert.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/serial_port.hpp>
+
+extern "C" {
+#include <fcntl.h>
+#include <getopt.h>
+#include <poll.h>
+#include <time.h>
+#include <errno.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <assert.h>
+#include <unistd.h> //  close
+#include <string.h> //  strerror
+}
+
+
+#include "my_robot/imu_filter_msg.h"
+
+#define IMU_JUMP_FRAME 600
+
+class IMU{
+    public:
+        IMU(double input_Q, double input_R);
+        ~IMU();
+        void imu_pub_func();
+        
+    private:
+    // varibles  
+        ros::NodeHandle n;
+        std::string name = ros::this_node::getName();
+        std::string port = "/dev/imu";
+        std::string model = "art_imu_02a";
+        int baud = 115200;
+        std::string frame_id = "IMU_link";
+        double delay;
+        boost::asio::io_service io_service;
+        boost::asio::serial_port* serial_port = 0;
+        int fd_ = -1;
+        ros::Publisher pub, pub_mag, pub_gps;
+        ros::Publisher imu_pub;
+
+        const uint8_t stop[6] = {0xA5, 0x5A, 0x04, 0x02, 0x06, 0xAA};
+        const uint8_t mode[6] = {0xA5, 0x5A, 0x04, 0x01, 0x05, 0xAA};
+        int data_length = 81;
+        uint8_t tmp[81];
+        uint8_t data_raw[200];
+        sensor_msgs::Imu msg;
+        sensor_msgs::MagneticField msg_mag;
+        sensor_msgs::NavSatFix msg_gps;
+        my_robot::imu_filter_msg imu_filter_msg;
+
+        int imu_init_jump_count = IMU_JUMP_FRAME;
+        double sum_x_acceleration = 0, sum_z_acceleration = 0;
+        double velociry_x = 0, velocity_z = 0;
+        double velociry_x_lase = 0, velocity_z_last = 0;
+
+        double angle_y = 0;
+
+        //paraments in UKF
+        double last_p = 0, now_p = 0;
+        double Q = 1, K = 0, R = 1.3;
+        double output = 0;
+
+        //paraments in file edit
+        // FILE* filterfile = fopen("filter.data", "r+");
+
+
+    
+    // functions 
+        int imu_init(){
+            try{
+                serial_port->open(port);
+            }
+            catch (boost::system::system_error &error){
+                ROS_ERROR("%s: Failed to open port %s with error %s",
+                        name.c_str(), port.c_str(), error.what());
+                return -1;
+            }
+
+            if (!serial_port->is_open()){
+                ROS_ERROR("%s: failed to open serial port %s",
+                        name.c_str(), port.c_str());
+                return -1;
+            }
+
+            typedef boost::asio::serial_port_base sb;
+
+            sb::baud_rate baud_option(baud);
+            sb::flow_control flow_control(sb::flow_control::none);
+            sb::parity parity(sb::parity::none);
+            sb::stop_bits stop_bits(sb::stop_bits::one);
+
+            serial_port->set_option(baud_option);
+            serial_port->set_option(flow_control);
+            serial_port->set_option(parity);
+            serial_port->set_option(stop_bits);
+
+            const char *path = port.c_str();
+            fd_ = open(path, O_RDWR);
+
+            if(fd_ < 0){
+                ROS_ERROR("Port Error!: %s", path);
+                return -1;
+            }
+
+            if(model == "art_imu_02a"){
+            write(fd_, stop, 6);
+            usleep(1000 * 1000);
+            write(fd_, mode, 6);
+            usleep(1000 * 1000);
+            data_length = 40;
+            }
+            ROS_WARN("Streaming Data...");
+
+        }
+
+        void imu_filter_func(double* ,int );
+        
+};
+
+
+#endif
---
+++ src/my_robot/src/imu_filter.cpp
@@ -0,0 +1,341 @@
+#include "stdlib.h"
+#include "my_robot/imu_filter.h"
+
+using std::cout;
+using std::endl;
+
+// FILE* originfile = fopen("test.data", "r+");
+
+
+static float d2f_acc(uint8_t a[2])
+{
+    int16_t acc = a[0];
+    acc = (acc << 8) | a[1];
+    return ((float) acc) / 16384.0f;
+}
+
+static float d2f_gyro(uint8_t a[2])
+{
+    int16_t acc = a[0];
+    acc = (acc << 8) | a[1];
+    return ((float) acc) / 32.8f;
+}
+
+static float d2f_mag(uint8_t a[2])
+{
+    int16_t acc = a[0];
+    acc = (acc << 8) | a[1];
+    return ((float) acc) / 1.0f;
+}
+
+static float d2f_euler(uint8_t a[2])
+{
+    int16_t acc = a[0];
+    acc = (acc << 8) | a[1];
+    return ((float) acc) / 10.0f;
+}
+
+static double d2f_latlon(uint8_t a[4])
+{
+    int64_t high = a[0];
+    high = (high << 8) | a[1];
+
+    int64_t low = a[2];
+    low = (low << 8) | a[3];
+    return (double)((high << 8) | low);
+}
+
+static double d2f_gpsvel(uint8_t a[2])
+{
+    int16_t acc = a[0];
+    acc = (acc << 8) | a[1];
+    return ((float) acc) / 10.0f;
+}
+
+static float d2ieee754(uint8_t a[4])
+{
+    union fnum {
+        float f_val;
+        uint8_t d_val[4];
+    } f;
+
+    memcpy(f.d_val, a, 4);
+    return f.f_val;
+}
+
+
+int uart_set(int fd, int baude, int c_flow, int bits, char parity, int stop)
+{
+    struct termios options;
+ 
+    if(tcgetattr(fd, &options) < 0)
+    {
+        perror("tcgetattr error");
+        return -1;
+    }
+
+    cfsetispeed(&options,B115200);
+    cfsetospeed(&options,B115200);
+
+    options.c_cflag |= CLOCAL;
+    options.c_cflag |= CREAD;
+
+    switch(c_flow)
+    {
+        case 0:
+            options.c_cflag &= ~CRTSCTS;
+            break;
+        case 1:
+            options.c_cflag |= CRTSCTS;
+            break;
+        case 2:
+            options.c_cflag |= IXON|IXOFF|IXANY;
+            break;
+        default:
+            fprintf(stderr,"Unkown c_flow!\n");
+            return -1;
+    }
+
+    switch(bits)
+    {
+        case 5:
+            options.c_cflag &= ~CSIZE;
+            options.c_cflag |= CS5;
+            break;
+        case 6:
+            options.c_cflag &= ~CSIZE;
+            options.c_cflag |= CS6;
+            break;
+        case 7:
+            options.c_cflag &= ~CSIZE;
+            options.c_cflag |= CS7;
+            break;
+        case 8:
+            options.c_cflag &= ~CSIZE;
+            options.c_cflag |= CS8;
+            break;
+        default:
+            fprintf(stderr,"Unkown bits!\n");
+            return -1;
+    }
+
+    switch(parity)
+    {
+        case 'n':
+        case 'N':
+            options.c_cflag &= ~PARENB;
+            options.c_cflag &= ~INPCK;
+            break;
+
+        case 's':
+        case 'S':
+            options.c_cflag &= ~PARENB;
+            options.c_cflag &= ~CSTOPB;
+            break;
+
+        case 'o':
+        case 'O':
+            options.c_cflag |= PARENB;
+            options.c_cflag |= PARODD;
+            options.c_cflag |= INPCK;
+            options.c_cflag |= ISTRIP;
+            break;
+
+        case 'e':
+        case 'E':
+            options.c_cflag |= PARENB;
+            options.c_cflag &= ~PARODD;
+            options.c_cflag |= INPCK;
+            options.c_cflag |= ISTRIP;
+            break;
+        default:
+            fprintf(stderr,"Unkown parity!\n");
+            return -1;
+    }
+
+    switch(stop)
+    {
+        case 1:
+            options.c_cflag &= ~CSTOPB;
+            break;
+        case 2:
+            options.c_cflag |= CSTOPB;
+            break;
+        default:
+            fprintf(stderr,"Unkown stop!\n");
+            return -1;
+    }
+
+    options.c_oflag &= ~OPOST;
+    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    options.c_cc[VTIME] = 0;
+    options.c_cc[VMIN] = 1;
+
+    tcflush(fd,TCIFLUSH);
+
+    if(tcsetattr(fd,TCSANOW,&options) < 0)
+    {
+        perror("tcsetattr failed");
+        return -1;
+    }
+
+    return 0;
+
+}
+
+
+IMU::IMU(double input_Q, double input_R){
+    /**************set element in UKF********************/
+    Q = input_Q;
+    R = input_R;
+    printf("Q = %f , R = %f \n", Q, R);
+    n.setParam("port", port);
+    n.setParam("model", model);
+    ROS_WARN("Model set to %s", model.c_str());
+    n.setParam("baud", baud);
+    ROS_WARN("Baudrate set to %d", baud);
+    n.param("frame_id", frame_id);
+    n.param("delay", delay, 0.0);
+    serial_port = new boost::asio::serial_port(io_service);
+    imu_init();
+    pub = n.advertise<sensor_msgs::Imu>("/imu_data", 1);
+    pub_mag = n.advertise<sensor_msgs::MagneticField>("mag", 1);
+    pub_gps = n.advertise<sensor_msgs::NavSatFix>("gps", 1);
+    imu_pub = n.advertise<my_robot::imu_filter_msg>("/imu_filter", 1);
+    // if (originfile == NULL){
+    //     cout<<"wrong when open file"<<endl;
+    // }
+    // else
+    //     cout<<"file opened"<<endl;
+};
+
+IMU::~IMU(){};
+
+void IMU::imu_pub_func(){
+    double vyaw_sum = 0;
+    double vyaw_bias = 0;
+    
+    while(n.ok()){
+        read(fd_, tmp, sizeof(uint8_t) * data_length);
+        memcpy(data_raw, tmp, sizeof(uint8_t) * data_length);
+        bool found = false;
+        int kk;
+        for(kk = 0; kk < data_length - 1; ++kk){
+            if(model == "art_imu_02a" && data_raw[kk] == 0xA5 && data_raw[kk + 1] == 0x5A){
+                unsigned char *data = data_raw + kk;
+                uint8_t data_length = data[2];
+                // --------------check
+                uint32_t checksum = 0;
+                for(int i = 0; i < data_length - 1  ; ++i)
+                    checksum += (uint32_t) data[i+2];
+
+                uint16_t check = checksum % 256;
+                uint16_t check_true = data[data_length+1];
+
+                if (check != check_true){
+                    printf("check error,please wait\n");
+                    continue;
+                }
+                Eigen::Vector3d ea0((-vyaw_bias-d2f_euler(data + 3)) * M_PI / 180.0,
+                                 0,
+                                  0);
+                Eigen::Matrix3d R;
+                R = Eigen::AngleAxisd(ea0[0], ::Eigen::Vector3d::UnitZ())
+                  * Eigen::AngleAxisd(ea0[1], ::Eigen::Vector3d::UnitY())
+                  * Eigen::AngleAxisd(ea0[2], ::Eigen::Vector3d::UnitX());
+                Eigen::Quaterniond q;
+                q = R;
+
+                msg.orientation.w = (double)q.w();
+                msg.orientation.x = (double)q.x();
+                msg.orientation.y = (double)q.y();
+                msg.orientation.z = (double)q.z();
+
+                msg.header.stamp = ros::Time::now();
+                msg.header.frame_id = frame_id;
+                msg.angular_velocity.x = d2f_gyro(data + 15);
+                msg.angular_velocity.y = d2f_gyro(data + 17);
+                msg.angular_velocity.z = d2f_gyro(data + 19);
+                msg.linear_acceleration.x = d2f_acc(data + 9) * 9.81;
+                msg.linear_acceleration.y = d2f_acc(data + 11) * 9.81;
+                msg.linear_acceleration.z = d2f_acc(data + 13) * 9.81;  
+                if (imu_init_jump_count == 0){
+                    printf("imu initlization DDONE!\n");
+                    printf("The parament is %f\n", sum_x_acceleration/IMU_JUMP_FRAME);
+                    imu_init_jump_count--;
+                }
+
+                if (imu_init_jump_count > 0){
+                    sum_x_acceleration += msg.angular_velocity.y;
+                    // sum_z_acceleration += msg.linear_acceleration.z;
+                    imu_init_jump_count--;
+                }
+                else{
+                    msg.angular_velocity.y -=  sum_x_acceleration/IMU_JUMP_FRAME;
+                    angle_y += msg.angular_velocity.y;
+                    // msg.linear_acceleration.z -=  sum_z_acceleration/IMU_JUMP_FRAME;
+                    imu_filter_msg.o_data = angle_y;
+                    imu_filter_func(&angle_y, 0);
+                    imu_filter_msg.f_data = angle_y;
+                }
+                // pub.publish(msg);
+                imu_pub.publish(imu_filter_msg);
+
+
+                // msg_mag.magnetic_field.x = d2f_mag(data + 21);
+                // msg_mag.magnetic_field.y = d2f_mag(data + 23);
+                // msg_mag.magnetic_field.z = d2f_mag(data + 25);
+                // msg_mag.header.stamp = msg.header.stamp;
+                // msg_mag.header.frame_id = msg.header.frame_id;
+                // pub_mag.publish(msg_mag);
+                found = true;
+            }
+        }
+    }
+    ROS_WARN("Wait 0.1s");
+    // fclose(originfile);
+    // fclose(filterfile);
+    ros::Duration(0.1).sleep();
+    ::close(fd_);
+    
+}
+
+void IMU::imu_filter_func(double* measurement, int filter_element_flag){
+    if (filter_element_flag == 0){//x axis
+        // cout<<"origin measurement = "<<*measurement;
+        // fseek(originfile, 0L, SEEK_END);
+        // fprintf(originfile, "%lf", *measurement);
+        // fputs(" ", originfile);
+        now_p = last_p + Q;
+        K = now_p/(now_p + R);
+        output += K * (*measurement - output);
+        last_p = now_p*(1 - K);
+        *measurement = output;
+        // cout<<"    measurement after filter = "<<*measurement<<endl;
+        // fseek(filterfile, 0L, SEEK_END);
+        // fprintf(filterfile, "%lf", *measurement);
+        // fputs(" ", filterfile);
+    }  
+}
+
+
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "imu_filter");
+    // ros::NodeHandle n;
+    if (argc != 3){
+        printf("please set Q and R\n");
+        return 0;
+    }
+    double q, r;
+    q = atof(*(argv + argc - 2));
+    r = atof(*(argv + argc - 1));
+    IMU imu_test(q, r);
+    imu_test.imu_pub_func();
+    ROS_WARN("imu_pub_func shut dhwn!");
+    // while(ros::ok()){
+
+    // }
+
+    return 0;
+}
